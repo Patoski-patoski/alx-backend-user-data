2e80cf2c56d80bb957ee998747bb65dae886fb98
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-06 04:27:24 +0000

    returns the decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """basic_auth.py module"""
 
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -9,6 +11,7 @@ class BasicAuth(Auth):
     Args:
         Auth (class): Base class
     """
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """extract_base64_authorization_header: extracts base64
@@ -23,6 +26,26 @@ class BasicAuth(Auth):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        after_basic = authorization_header.split("Basic")[1].strip()
 
+        after_basic = authorization_header.split("Basic")[1].strip()
         return after_basic
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """decode_base64_authorization_header: decoded value of a Base64 string
+        Args:
+            base64_authorization_header (str): authorization header
+        Returns:
+            str: the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            valid_base64 = base64.b64decode(base64_authorization_header)
+            return valid_base64.decode()
+        except binascii.Error:
+            return None
