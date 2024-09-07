219bc73f5be0fbc989843853f5789e3ff09a29b2
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-07 21:10:49 +0000

    add a new endpoint: GET /users/me to retrieve the authenticated User object.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,8 +1,5 @@
-# api/vi/app.py
-# !/usr/bin/env python3
-"""
-Route module for the API
-"""
+#!/usr/bin/env python3
+"""Route module for the API"""
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
