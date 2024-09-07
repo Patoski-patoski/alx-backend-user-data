904e3383fb151e899a256a0a17d5ab0c5850d047
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-07 22:53:43 +0000

    Update SessionAuth class: Create a class attribute and instance method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """session auth.py module"""
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +10,18 @@ class SessionAuth(Auth):
     Args:
         Auth (class): Base class
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id
+        Args:
+            user_id (str, optional): Defaults to None.
+        Returns:
+            str: sesssion id or None
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
