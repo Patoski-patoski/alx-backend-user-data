afc3b65cd8e7812483e8592b335c77f3c0169502
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-08 00:26:24 +0000

    Returns a User instance based on a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -36,3 +37,12 @@ class SessionAuth(Auth):
         if session_id is None and not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance
+        Args:
+            request (optional): Defaults to None.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
