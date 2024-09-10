a397824e903e11b72b9f6338adace51f62bcf045
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-10 19:46:41 +0000

    setup flask server

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""app module"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def set_up():
+    """return a JSON payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
