63fde5451af7107a8f064d3b012c3a729f7f8331
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-11 20:42:40 +0000

    addedd logout functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def login() -> str:
 
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout():
+def logout() -> str:
     """logout: User logout function"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
