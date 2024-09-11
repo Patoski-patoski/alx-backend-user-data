1eb75a2f358816d726f8adea40a66bce2e9804b7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-11 20:52:44 +0000

    edited Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,7 +56,7 @@ def logout() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for('set_up'))
+        return redirect(url_for("index"))
     else:
         abort(403)
 
