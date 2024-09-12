4b428a98f98f90a32aafe6a0adb8a5594f245463
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-12 02:47:33 +0000

    edited logout()

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,7 +54,7 @@ def logout():
     """logout: User logout function"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user.session_id is None:
         abort(403)
     else:
         AUTH.destroy_session(user.id)
