e3a520e307fe438e9bedfd89b3e85f537b7b52cb
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-12 04:33:38 +0000

    Update password end-point, 3

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,6 +93,8 @@ def update_password():
         user = AUTH._db.find_user_by(email=email)
         if user is None or user.reset_token is None:
             abort(403)
+        if user.reset_token is reset_token:
+            abort(403)
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
